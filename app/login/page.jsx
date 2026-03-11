17750497767163094240d4cae02e29ed255863db
Jean Carlos Pereira Souza <2024108102540017@ifgoiano.local>
2026-03-11 08:43:19 -0300

    adicionei a pagina Mauro

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,9 +1,10 @@
 import LoginPage from "../../components/LoginPage";
-
+import Mauro from "../../components/Mauro";
 export default function login(){
     return(
         <>
             <LoginPage/>
+            <Mauro></Mauro>
         </>
     )
 }
