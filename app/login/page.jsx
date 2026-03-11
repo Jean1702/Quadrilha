e7b9619e77d2cd5b5efcebc2e64157a0215de824
Jean1702 <jean.carlos1@estudante.ifgoiano.edu.br>
2026-03-11 08:45:32 -0300

    Remove Mauro component from login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,10 +1,8 @@
 import LoginPage from "../../components/LoginPage";
-import Mauro from "../../components/Mauro";
 export default function login(){
     return(
         <>
             <LoginPage/>
-            <Mauro></Mauro>
         </>
     )
 }
