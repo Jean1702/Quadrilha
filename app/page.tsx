2c7d36db12d033d20009fd36bd9894959d2e42b9
Zoidevzx <opala2019@gmail.com>
2026-05-07 21:04:34 -0300

    Corrigir erro de deploy no Vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import HomePage from '@/components/HomePage';
-import {CreateClient} from "@/lib/supabase/server.ts"
+import { CreateClient } from "@/lib/supabase/server"
 export default async function Home() {
 
   const supabase = await CreateClient()
