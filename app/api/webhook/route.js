56564b1c402ce64a9a90f326622ccf78e890d998
Jean1702 <jean.carlos1@estudante.ifgoiano.edu.br>
2026-05-29 17:20:55 -0300

    resolvendo deploy

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { CreateClient } from '../../../lib/supabase/server';
+import {CreateClient} from "../../lib/supabase/server.ts";
 import { MercadoPagoConfig, Payment } from 'mercadopago';
 
 export async function POST(request) {
